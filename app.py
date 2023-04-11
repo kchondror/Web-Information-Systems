275c6bdd96a19c44a5f2b3f912c829d9c6c47bcf
Konstantinos Mazgaltzidis <kamazgal@csd.auth.gr>
2023-04-11 19:43:25 +0300

    Finished crawler.

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 # BEGIN CODE HERE
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 from pymongo import TEXT
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+options = Options()
+options.headless = True
+driver = webdriver.Chrome(options=options)
+driver.get("https://qa.auth.gr/el/x/studyguide/600000438/current")
+
 # END CODE HERE
 
 app = Flask(__name__)
@@ -36,5 +46,16 @@ def content_based_filtering():
 @app.route("/crawler", methods=["GET"])
 def crawler():
     # BEGIN CODE HERE
-    return ""
+    try:
+        semester = request.args.get("semester")
+        xpath = f"//*[@id='exam{semester}']/tbody/tr"
+        elements = driver.find_elements(By.XPATH, xpath)
+        course_titles = [_.get_attribute('coursetitle') for _ in elements]
+
+        if len(course_titles ) == 0:
+            return "BAD REQUEST", 400
+        return jsonify(course_titles), 200
+
+    except Exception as e:
+        return "BAD REQUEST", 400
     # END CODE HERE
